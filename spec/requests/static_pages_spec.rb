d7c1e9f138b210ac3eb0f7d53c81c42db98968dc
Callum <callumdryden@gmail.com>
2012-06-25 16:21:45 +0000

    Adding the second excercise

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+let(:title) { "Ruby on Rails Tutorial Sample App | " }
 
   describe "Home page" do
 
@@ -12,7 +13,7 @@ describe "Static pages" do
      it "should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => title + "Home")
     end
   end
  
@@ -26,7 +27,7 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Help")
+                        :text => title + "Help")
     end
   end
 
@@ -41,7 +42,7 @@ describe "Static pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | About Us")
+                    :text => title + "About Us")
     end
   end
 
@@ -55,7 +56,7 @@ describe "Static pages" do
    it "should have the title 'Contact'" do
      visit '/static_pages/contact'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | Contact")
+                    :text => title + "Contact")
     end
   end
 
