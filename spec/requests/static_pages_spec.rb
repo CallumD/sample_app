14e213334f0e535ee99b254f38b0382f92cf22a2
Callum <callumdryden@gmail.com>
2012-06-25 16:09:57 +0000

    Adding the excecise

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,7 @@ describe "Static pages" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
 
      it "should have the title 'Home'" do
@@ -20,7 +20,7 @@ describe "Static pages" do
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
@@ -34,7 +34,8 @@ describe "Static pages" do
 
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'About Us')
+      #page.should have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
@@ -43,4 +44,19 @@ describe "Static pages" do
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact page" do
+
+   it "should have the content 'Contact'" do
+     visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+   end
+  
+   it "should have the title 'Contact'" do
+     visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
 end
