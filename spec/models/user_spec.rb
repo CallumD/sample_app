010e16a33555dbdc55d1569d7e0ab0e99342c0c3
Callum <callumdryden@gmail.com>
2012-07-28 10:00:13 +0000

    Updating to rails 3.2.7

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,7 +43,10 @@ describe User do
 
 
   describe "with admin attribute set to 'true'" do
-    before { @user.toggle!(:admin) }
+    before do
+      @user.save 
+      @user.toggle!(:admin) 
+    end
 
     it {should be_admin }
   end
