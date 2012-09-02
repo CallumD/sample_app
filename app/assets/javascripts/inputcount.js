0aead53d8de98fd8e3e613cd3d2df235d4e4e0d5
Callum Dryden <callumdryden@gmail.com>
2012-09-02 14:23:13 +0100

    Tidying up some formatting and completing the excercise for chapter 10.

--- app/assets/javascripts/inputcount.js
+++ app/assets/javascripts/inputcount.js
@@ -3,22 +3,26 @@ $(document).ready(function(){
  * Character Counter for inputs and text areas
  */
 $('.word_count').each(function(){
+        
         // get current number of characters
-        var length = 140 - $(this).val().length;
+        var length = 140 - $(this).val().length,
+        text = ' characters remaining';
         // get current number of words
         //var length = $(this).val().split(/\b[\s,\.-:;]*/).length;
         // update characters
-        $(this).parent().find('.counter').html( length + ' characters');
+        $(this).parent().find('.counter').html( length + text);
         // bind on key up event
         $(this).keyup(function(){
                 // get new length of characters
                 var new_length = 140 - $(this).val().length;
-                $(this).parent().find('.counter').html( new_length + ' characters');
+                $(this).parent().find('.counter').html( new_length + text);
 
                 if (new_length < 0){
 		  $(this).parent().find('.post').attr("disabled", true);
+		  $(this).parent().find('.counter').css('color', 'red');
                 } else {
 		  $(this).parent().find('.post').attr("disabled", false);
+		  $(this).parent().find('.counter').css('color','');
                 }
         });
     });
