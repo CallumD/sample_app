abc5e2bc9f786b8bcdf466df1193f94d42aabcb6
Callum Dryden <c.dryden@intrallect.com>
2012-09-10 12:28:16 +0100

    Committing up to 11.2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,6 +33,8 @@ describe User do
   it { should respond_to(:feed) }
   it { should respond_to(:relationships) }
   it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
   it { should respond_to(:following?) }
   it { should respond_to(:follow!) }
   it { should respond_to(:unfollow!) }
