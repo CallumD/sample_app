991cf13d1c1f627a47ddeedb50c4fe0b8a1d82f0
Callum Dryden <callumdryden@gmail.com>
2012-09-23 15:29:50 +0100

    Committing up to 11.2.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
   
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_filter :correct_user, only: [:edit, :update]
   before_filter :admin_user, only: :destroy
   before_filter :non_signed_in, only: [:new, :create]
 
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
 
   def destroy
     user_to_remove = User.find(params[:id])
@@ -51,7 +64,7 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+  
   private
 
     def non_signed_in
